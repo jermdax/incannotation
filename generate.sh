c928675eb4417d9a663277a388287fddc8a43c17
jermdax <jdavidquinn@hotmail.com>
2016-07-29 22:57:31 +0100

    now copies into client and server  directories

--- generate.sh
+++ generate.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+GEN_DIR_CL=incannotation_cl
+GEN_DIR_SV=incannotation_svr
+GEN_SCRIPT='generate.sh'
 if [ -d $1 ]; then
-  mkdir `pwd`/generated
-  cp -r $(ls $1 | grep -v 'generated$' | grep -v 'generate.sh$') generated
+  #create the destination directory from where this is running
+  mkdir `pwd`/$GEN_DIR
+  #copy the files from the given directory into the client generated director
+  cp -r $(ls $1 | grep -v $GEN_DIR_CL'$' | grep -v $GEN_DIR_SV $GEN_DIR_CL)
+  #copy the files from the given directory into the client generated director
+  cp -r $(ls $1 | grep -v $GEN_DIR_SV'$' | grep -v $GEN_DIR_SV $GEN_DIR_SV)
 fi
