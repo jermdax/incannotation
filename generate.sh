45927793a36196329a6de42fce0bad46e7176a69
jermdax <jdavidquinn@hotmail.com>
2016-07-29 23:00:46 +0100

    fixed syntax errors

--- generate.sh
+++ generate.sh
@@ -5,9 +5,10 @@ GEN_DIR_SV=incannotation_svr
 GEN_SCRIPT='generate.sh'
 if [ -d $1 ]; then
   #create the destination directory from where this is running
-  mkdir `pwd`/$GEN_DIR
+  mkdir `pwd`/$GEN_DIR_CL
+  mkdir `pwd`/$GEN_DIR_SV
   #copy the files from the given directory into the client generated director
-  cp -r $(ls $1 | grep -v $GEN_DIR_CL'$' | grep -v $GEN_DIR_SV $GEN_DIR_CL)
+  cp -r $(ls $1 | grep -v $GEN_DIR_CL'$' | grep -v $GEN_DIR_SV) $GEN_DIR_CL
   #copy the files from the given directory into the client generated director
-  cp -r $(ls $1 | grep -v $GEN_DIR_SV'$' | grep -v $GEN_DIR_SV $GEN_DIR_SV)
+  cp -r $(ls $1 | grep -v $GEN_DIR_SV'$' | grep -v $GEN_DIR_SV) $GEN_DIR_SV
 fi
