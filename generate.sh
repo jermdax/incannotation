1ee96706c27a728878362356125e76604a4f6826
jermdax <jdavidquinn@hotmail.com>
2016-07-30 11:31:05 +0100

    added submodules and continued work

--- generate.sh
+++ generate.sh
@@ -3,12 +3,29 @@
 GEN_DIR_CL=incannotation_cl
 GEN_DIR_SV=incannotation_svr
 GEN_SCRIPT='generate.sh'
+LOCATIONS_FILE=token_occurances
+FILES_OF_INTEREST=*.c
+
 if [ -d $1 ]; then
+  #cleanup old resource from last time it was ran
+  if [ -d $GEN_DIR_CL ]; then
+    rm -rf $GEN_DIR_CL
+  fi
+  if [ -d $GEN_DIR_SV ]; then
+    rm -rf $GEN_DIR_SV
+  fi
+
   #create the destination directory from where this is running
   mkdir `pwd`/$GEN_DIR_CL
   mkdir `pwd`/$GEN_DIR_SV
   #copy the files from the given directory into the client generated director
-  cp -r $(ls $1 | grep -v $GEN_DIR_CL'$' | grep -v $GEN_DIR_SV) $GEN_DIR_CL
+  cp -r $(ls $1 | grep -v $GEN_DIR_CL'$' | grep -v $GEN_DIR_SV'$') $GEN_DIR_CL
   #copy the files from the given directory into the client generated director
-  cp -r $(ls $1 | grep -v $GEN_DIR_SV'$' | grep -v $GEN_DIR_SV) $GEN_DIR_SV
+  cp -r $GEN_DIR_CL/* $GEN_DIR_SV
+
+  #grep for the files of interest
+  grep -rn "S_VAR" $GEN_DIR_CL > /tmp/$LOCATIONS_FILE --include $FILES_OF_INTEREST
+  cp /tmp/$LOCATIONS_FILE $GEN_DIR_SV/$LOCATIONS_FILE
+  grep -rn "S_VAR" $GEN_DIR_SV > /tmp/$LOCATIONS_FILE --include $FILES_OF_INTEREST
+  cp /tmp/$LOCATIONS_FILE $GEN_DIR_CL/$LOCATIONS_FILE
 fi
---
+++ test/testSrc/test1.c
@@ -0,0 +1,11 @@
+#define S_VAR
+
+S_VAR int x = 0;
+
+S_VAR
+int y;
+
+S_VAR int main(int argc, char* argv[])
+{
+  return 0;
+}
